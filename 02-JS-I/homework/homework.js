8ff0236ee9656fc41be217b7082358e05c5e70d9
Manuel <manu.beni009@gmail.com>
2022-08-17 19:21:20 -0300

    bueno

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -68,15 +68,11 @@ function sonIguales(x, y) {
     return false
     }
   }
-  
-  
-}
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
 }
 
 function menosQueNoventa(num) {
